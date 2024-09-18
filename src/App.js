7fca65d17cfb54230f12ce8040996c5b9d2008fe
rahulk67 <rahulkumawat6767@gmail.com>
2024-09-18 11:13:43 +0530

    commit message here

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './components/pages/HomePage';
+import DealsPage from './components/pages/DealsPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/pages/Layout';
+import SignPage from './components/auth/SignPage';
+import UserForm from './components/auth/UserForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="deals" element={<DealsPage />} />
+          <Route path="sign-in" element={<SignPage />} />
+          <Route path="user-form" element={<UserForm />} />
+
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/pages/DealsPage.js
@@ -0,0 +1,104 @@
+import React from 'react'
+
+const DealsPage = () => {
+    return (
+        <div>
+
+            <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+                <div class="bg-gray-100 p-4">
+                    <section class="py-8 bg-white md:py-16 dark:bg-gray-900 antialiased">
+                        <div class="max-w-screen-sm px-4 mx-auto 2xl:px-0">
+                            <div class="lg:grid md:grid-cols-2 md:gap-8 xl:gap-16">
+                                <div class="shrink-0 max-w-md md:max-w-sm mx-auto">
+                                    <img class="w-full" src="./motorola.png" alt="" />
+
+                                </div>
+
+                                <div class="mt-6 sm:mt-8 lg:mt-0">
+                                    <h1
+                                        class="text-sm font-semibold text-gray-900 sm:text-xl dark:text-white"
+                                    >
+                                        Apple iMac 24" All-In-One Computer, Apple M1, 8GB RAM, 256GB SSD,
+                                        Mac OS, Pink
+                                    </h1>
+                                    <div class="mt-4 sm:items-center sm:gap-4 sm:grid">
+                                        <p
+                                            class="text-xl font-extrabold text-gray-900 md:text-xl dark:text-white"
+                                        >
+                                            $1,249.99
+                                        </p>
+
+                                        <p
+                                            class="text-sm  text-gray-900 sm:text-sm dark:text-white"
+                                        >
+                                            From the card of ICICI
+                                        </p>
+
+
+                                    </div>
+
+                                    <div class="mt-6 sm:gap-4 sm:items-center sm:flex sm:mt-8">
+
+                                        {/* <a
+                                    href="#"
+                                    title=""
+                                    class="text-white mt-4 sm:mt-0 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 bg-primary-700 focus:outline-none ring-primary-800 flex items-center justify-center bg-black"
+                                    role="button"
+                                >
+                                    <svg
+                                        class="w-5 h-5 -ms-2 me-2"
+                                        aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="24"
+                                        height="24"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <path
+                                            stroke="currentColor"
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6"
+                                        />
+                                    </svg>
+
+                                    Buy Now
+                                </a> */}
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+
+                <div class="bg-gray-200 p-4">
+
+
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+
+    )
+}
+
+export default DealsPage
