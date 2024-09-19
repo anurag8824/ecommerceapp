c211e9c16f5fcdcba6f1127114dd466dd600c3de
rahulk67 <rahulkumawat6767@gmail.com>
2024-09-19 13:01:34 +0530

    commit message here

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/pages/HomePage';
 import DealsPage from './components/pages/DealsPage';
@@ -6,6 +5,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/pages/Layout';
 import SignPage from './components/auth/SignPage';
 import UserForm from './components/auth/UserForm';
+import ProfilePage from './components/pages/ProfilePage';
+import ProductPage from './components/pages/ProductPage';
+import AdminPage from './components/adminpanel/AdminPage';
+import DashBoard from './components/adminpanel/DashBoard';
 
 function App() {
   return (
@@ -16,6 +19,10 @@ function App() {
           <Route path="deals" element={<DealsPage />} />
           <Route path="sign-in" element={<SignPage />} />
           <Route path="user-form" element={<UserForm />} />
+          <Route path="profile" element={<ProfilePage />} />
+          <Route path="product" element={<ProductPage />} />
+          <Route path="admin" element={<AdminPage />} />
+          <Route path="dashboard" element={<DashBoard />} />
 
           {/* <Route path="*" element={<NoPage />} /> */}
         </Route>
--- src/components/auth/SignPage.js
+++ src/components/auth/SignPage.js
@@ -1,32 +1,89 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
 
 const SignPage = () => {
 
     const [state, setState] = useState(true)
+    const [Email, setEmail] = useState("");
+    const [otp , setOtp] = useState("");
+
+
     const navigate = useNavigate();
 
-    function showForm(event) {
+
+    // useEffect(() => {
+    const showForm = (event) => {
+
         event.preventDefault();
         // console.log("onclick triggered")
-        setState(false)
+      
+        console.log(Email)
+
+        axios.post('http://localhost:8000/user/EmailRegister', { Email })
+
+
+            .then((res) => {
+                console.log(res);
+                const mes = res.data.message
+                if (mes == "user already exist!") {
+                    alert(res.data.message)
+                } else if (mes == "Error is sending Email !") {
+                    alert(res.data.message)
+
+                } else {
+                    setState(false)
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            })
     }
 
+
+    // }, [])
+
+
+
+
     const handleSubmit = (event) => {
+
         event.preventDefault(); // Prevents default form submission behavior
+
+
 
         navigate('/user-form');
     }
 
+    // useEffect(() => {
+
+    //     // axios.get('')
+    //     // .then(response => {
+    //     //     // Handle success
+    //     //     setData(response.data);
+    //     //     setLoading(false);
+    //     //   })
+    //     //   .catch(error => {
+    //     //     // Handle error
+    //     //     setError(error);
+    //     //     setLoading(false);
+    //     //   });
+
+
+
+    // }, [])
+
+
     return (
         <div>
-            <div class="w-full max-w-6xl mx-auto px-4 md:px-6 py-24"> 
+            <div class="w-full max-w-6xl mx-auto px-4 md:px-6 py-24">
                 <div class={`justify-center ${state === true ? "block" : "hidden"}`}>
                     <form class="max-w-sm mx-auto">
                         <div class="mb-5">
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter Your email</label>
-                            <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  placeholder="name@xyz1234.com" required />
+                            <input type="email" id="email" onChange={e => setEmail(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@xyz1234.com" required />
                         </div>
 
                         <button type="submit" onClick={showForm} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send OTP</button>
@@ -61,8 +118,8 @@ const SignPage = () => {
                                     maxlength="1" />
                             </div>
                             <div class="max-w-[260px] mx-auto mt-4">
-                                <button type="submit" 
-                                
+                                <button type="submit"
+
                                     class="w-full inline-flex justify-center whitespace-nowrap rounded-lg bg-blue-700 px-3.5 py-2.5 text-sm font-medium text-white shadow-sm shadow-indigo-950/10 hover:bg-indigo-600 focus:outline-none focus:ring focus:ring-indigo-300 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 transition-colors duration-150">Verify
                                     Account</button>
                             </div>
